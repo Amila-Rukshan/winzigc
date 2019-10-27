d509c373547d2098a7aaf3445efeae8a216f8718
amila rukshan <amila.15@cse.mrt.ac.lk>
2019-10-28 00:20:43 +0530

    token generation

--- src/winzigc.java
+++ src/winzigc.java
@@ -8,8 +8,15 @@
  */
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class winzigc {
+
+    private static List<SyntaxToken> tokenStream = new ArrayList<SyntaxToken>();
+
     public static void main(String[] args) {
         String flag = args[0];
         switch (flag){
@@ -24,9 +31,20 @@ public class winzigc {
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
-                System.out.println(program_as_a_string.charAt(program_as_a_string.length()-1)=='\n');
 
                 Lexer lexer = new Lexer(program_as_a_string);
+
+                SyntaxKind kind;
+                do{
+                    SyntaxToken token = lexer.findNextToken();
+                    tokenStream.add(token);
+                    kind = token.kind;
+                }
+                while(kind != SyntaxKind.EndOfProgramToken & kind != SyntaxKind.BadToken);
+
+                for(SyntaxToken token: tokenStream){
+                    System.out.println(token.kind+" : "+token.position+" >"+token.text+"<");
+                }
 
                 break;
             case "-codegen":
@@ -64,8 +82,8 @@ public class winzigc {
 }
 
 enum SyntaxKind {
-    //     for the lexer
-    IdentifierToken(),
+    // for the lexer
+    IdentifierToken,
     IntegerToken,
     WhiteSpaceToken,
     CharToken,
@@ -123,20 +141,36 @@ enum SyntaxKind {
     PlusToken,                  //  +
     MinusToken,                 //  -
     MultiplyToken,              //  *
-    DivideToken                 //  /
+    DivideToken,                //  /
+    EndOfProgramToken,
+    BadToken
 
+
+//    private final Pattern pattern;
+//
+//    SyntaxKind(String regex) {
+//        pattern = Pattern.compile("^" + regex);
+//    }
+//
+//    int endOfMatch(String s) {
+//        Matcher m = pattern.matcher(s);
+//
+//        if (m.find()) {
+//            return m.end();
+//        }
+//        return -1;
+//    }
 }
 
 class SyntaxToken {
     SyntaxKind kind;
     int position;
     String text;
-    Object value;
-    public SyntaxToken(SyntaxKind kind, int position, String text, Object value){
+
+    public SyntaxToken(SyntaxKind kind, int position, String text){
         this.kind = kind;
         this.position = position;
         this.text = text;
-        this.value = value;
     }
 }
 
@@ -144,8 +178,109 @@ class Lexer{
 
     private final String _text;
 
+    private int _position;
+
+    private char getCurrentChar(){
+        if(_position >= _text.length())
+            return '\0';
+        return _text.charAt(_position);
+    }
+
     public Lexer(String text){
         _text = text;
+    }
+
+    private void Next(){
+        _position += 1;
+    }
+
+    public SyntaxToken findNextToken(){
+        if(_position >= _text.length()){
+            return new SyntaxToken(SyntaxKind.EndOfProgramToken, _position, "\0");
+        }
+
+        Pattern pattern;
+        String remaining_text;
+        Matcher m;
+
+        // find identifiers and alphabetic syntax token
+        pattern = Pattern.compile("^[A-Za-z_][A-Za-z0-9_]*");
+        remaining_text = _text.substring(_position, _text.length());
+        m = pattern.matcher(remaining_text);
+
+        if(m.find()){
+            int start = _position;
+            int end = m.end();
+            _position += end;
+
+            String lexeme = remaining_text.substring(0, end);
+            switch(lexeme){
+                case "program": return new SyntaxToken(SyntaxKind.ProgramToken, start, lexeme);
+                case "var": return new SyntaxToken(SyntaxKind.VarToken, start, lexeme);
+                case "const": return new SyntaxToken(SyntaxKind.ConstToken, start, lexeme);
+                case "type": return new SyntaxToken(SyntaxKind.TypeToken, start, lexeme);
+                case "function": return new SyntaxToken(SyntaxKind.FunctionToken, start, lexeme);
+                case "return": return new SyntaxToken(SyntaxKind.ReturnToken, start, lexeme);
+                case "begin": return new SyntaxToken(SyntaxKind.BeginToken, start, lexeme);
+                case "end": return new SyntaxToken(SyntaxKind.EndToken, start, lexeme);
+                case "output": return new SyntaxToken(SyntaxKind.OutputToken, start, lexeme);
+                case "if": return new SyntaxToken(SyntaxKind.IfToken, start, lexeme);
+                case "then": return new SyntaxToken(SyntaxKind.ThenToken, start, lexeme);
+                case "else": return new SyntaxToken(SyntaxKind.ElseToken, start, lexeme);
+                case "while": return new SyntaxToken(SyntaxKind.WhileToken, start, lexeme);
+                case "do": return new SyntaxToken(SyntaxKind.DoToken, start, lexeme);
+                case "case": return new SyntaxToken(SyntaxKind.CaseToken, start, lexeme);
+                case "of": return new SyntaxToken(SyntaxKind.OfToken, start, lexeme);
+                case "otherwise": return new SyntaxToken(SyntaxKind.OtherwiseToken, start, lexeme);
+                case "repeat": return new SyntaxToken(SyntaxKind.RepeatToken, start, lexeme);
+                case "for": return new SyntaxToken(SyntaxKind.ForToken, start, lexeme);
+                case "until": return new SyntaxToken(SyntaxKind.UntilToken, start, lexeme);
+                case "loop": return new SyntaxToken(SyntaxKind.LoopToken, start, lexeme);
+                case "pool": return new SyntaxToken(SyntaxKind.PoolToken, start, lexeme);
+                case "exit": return new SyntaxToken(SyntaxKind.ExitToken, start, lexeme);
+                case "mod": return new SyntaxToken(SyntaxKind.ModulusOprToken, start, lexeme);
+                case "or": return new SyntaxToken(SyntaxKind.OrOprToken, start, lexeme);
+                case "and": return new SyntaxToken(SyntaxKind.AndOprToken, start, lexeme);
+                case "not": return new SyntaxToken(SyntaxKind.NotOprToken, start, lexeme);
+                case "read": return new SyntaxToken(SyntaxKind.ReadToken, start, lexeme);
+                case "succ": return new SyntaxToken(SyntaxKind.SuccessorToken, start, lexeme);
+                case "pred": return new SyntaxToken(SyntaxKind.PredecessorToken, start, lexeme);
+                case "chr": return new SyntaxToken(SyntaxKind.CharFuncToken, start, lexeme);
+                case "ord": return new SyntaxToken(SyntaxKind.OrdinalFuncToken, start, lexeme);
+                case "eof": return new SyntaxToken(SyntaxKind.EndOfFileToken, start, lexeme);
+                default: return new SyntaxToken(SyntaxKind.IdentifierToken, start, lexeme);
+            }
+        }
+
+        // white spaces
+        pattern = Pattern.compile("^(\\s|\\v|\\h|\\n)+");
+        remaining_text = _text.substring(_position, _text.length());
+        m = pattern.matcher(remaining_text);
+
+        if(m.find()){
+            int start = _position;
+            int end = m.end();
+            _position += end;
+
+            String lexeme = remaining_text.substring(0, end);
+            return new SyntaxToken(SyntaxKind.WhiteSpaceToken, start, lexeme);
+        }
+
+        // integers
+        pattern = Pattern.compile("^[\\d]+");
+        remaining_text = _text.substring(_position, _text.length());
+        m = pattern.matcher(remaining_text);
+
+        if(m.find()){
+            int start = _position;
+            int end = m.end();
+            _position += end;
+
+            String lexeme = remaining_text.substring(0, end);
+            return new SyntaxToken(SyntaxKind.IntegerToken, start, lexeme);
+        }
+
+        return new SyntaxToken(SyntaxKind.BadToken, _position, null);
     }
 
 }
